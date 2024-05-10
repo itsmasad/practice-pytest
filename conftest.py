7a7d42d8d61a76434fe7eeeed22aaf99ad080605
itsmasad <muhammadasadofficial@gmail.com>
2024-05-10 15:46:39 +0500

    my first commit

---
+++ conftest.py
@@ -0,0 +1,15 @@
+# import pytest
+# from selenium import webdriver
+
+
+# @pytest.fixture(scope="function",autouse=True)
+# def setUp():
+#     driver = webdriver.Chrome()
+#     yield driver
+#     driver.close()
+
+# driver = webdriver.Chrome()
+
+# driver.execute_script()
+
+
---
+++ test_login.py
@@ -0,0 +1,35 @@
+# import pytest
+# import time
+# from testData import data
+
+# @pytest.mark.smock()
+# def test_login(setUp):
+#     driver = setUp
+#     driver.get("https://www.google.com/")
+#     driver.implicitly_wait
+#     driver.maximize_window
+    
+ 
+# @pytest.mark.parametrize("name,age",data)
+# def test_para(name,age):
+#     print(name+' '+age)
+
+
+
+import openpyxl.workbook
+import pytest
+import openpyxl
+
+def read_data():
+    workbook = openpyxl.load_workbook("workbook.xlsx")
+    sheet = workbook.active
+    test_data = []
+    for row in sheet.iter_rows(min_row=2,min_col=1,max_row=4,max_col=2,values_only=True):
+        username,password = row
+        test_data.append((username,password))
+    return test_data
+
+
+@pytest.mark.parametrize("Username,Password",read_data())
+def test_Para(Username,Password):
+    print(f'{Username} & {Password}')
